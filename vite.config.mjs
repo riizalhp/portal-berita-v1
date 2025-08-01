170549d2016188f04280cf4478d9bdba2dad08cc
riizalhp <rizalhp12345@gmail.com>
2025-08-01 11:36:44 +0700

    siap deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,10 +5,9 @@ import tagger from "@dhiwise/component-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
-  // comment this out if that isn't relevant for your project
+  // Using standard dist output directory for deployment compatibility
   build: {
-    outDir: "build",
+    outDir: "dist",
     chunkSizeWarningLimit: 2000,
   },
   plugins: [tsconfigPaths(), react(), tagger()],
